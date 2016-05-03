a08805c57be6e74ddf6debaf5ef3c77695c44461
Paul van Zyl <paul@nonacreative.com>
2016-05-03 18:55:34 +0200

    release 0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 Package.describe({
   name: 'pushplaybang:holster',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Easy placeholder templates for Blaze inspired by facebooks loading pattern',
   git: 'https://github.com/Pushplaybang/holster',
   documentation: 'README.md'
